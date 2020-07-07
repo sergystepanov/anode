efea62bacf50f6d677ae84a916928f9ecf8914e5
oimate <sergystepanov@gmail.com>
2020-07-07 22:11:41 +0300

    Update Socket module usage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import Socket from './network/socket';
 const address =
   ((window.location.protocol == 'https:' && 'wss://') || 'ws://') + window.location.host + '/ws/';
 
-const ws = new Socket(address);
+const ws = Socket(address);
 
 ws.send('a');
 ws.send('b');
--- frontend/src/network/socket.js
+++ frontend/src/network/socket.js
@@ -1,39 +1,47 @@
 /**
  * WebSocket connections module.
  *
- * @version 1
+ * @param {string} address A WebSocket address value.
+ * @example <caption>Example usage of socket module.</caption>
+ *
+ * import {Socket} from './socket'
+ *
+ * const conn = Socket('ws://localhost:1234/ws');
+ * conn.send('test');
+ *
  */
 export default function socket(address = '') {
   const messages = [];
-  let opened = false;
 
   console.info(`[socket] connecting to [${address}]`);
 
-  const connection = new WebSocket(address);
-  connection.bufferType = 'arraybuffer';
+  const conn = new WebSocket(address);
+  conn.binaryType = 'arraybuffer';
 
-  connection.onopen = () => {
+  conn.onopen = () => {
     console.info('[socket] connection open');
-    opened = true;
-
     console.info(`[socket] there are ${messages.length} messages in queue`);
 
     let message;
     while ((message = messages.pop())) {
-      connection.send(message);
+      conn.send(message);
     }
   };
-  connection.onerror = (error) => console.error(`[socket] ${error}`);
-  connection.onclose = () => console.info('[socket] closed');
-  connection.onmessage = (response) => {
+  conn.onerror = (error) => console.error(`[socket] ${error}`);
+  conn.onclose = () => console.info('[socket] closed');
+  conn.onmessage = (response) => {
     console.log(response);
   };
 
-  const send = (text) => {
-    if (!opened) {
-      messages.push(text);
+  /**
+   * Sends a message into the socket.
+   * @param {string | ArrayBuffer} data Some data value to send into the socket.
+   */
+  const send = (data) => {
+    if (conn.readyState !== WS_STATE.OPEN) {
+      messages.push(data);
     } else {
-      connection.send(text);
+      conn.send(data);
     }
   };
 
@@ -41,3 +49,15 @@ export default function socket(address = '') {
     send,
   };
 }
+
+// socket states
+const WS_STATE = Object.freeze({
+  // Socket has been created. The connection is not yet open.
+  CONNECTING: 0,
+  // The connection is open and ready to communicate.
+  OPEN: 1,
+  // The connection is in the process of closing.
+  CLOSING: 2,
+  // The connection is closed or couldn't be opened.
+  CLOSED: 3,
+});
